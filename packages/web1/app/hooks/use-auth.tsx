a3d1e64283f0a76fa86394fa6b3e6049d24c6b9f
herman <hgeldenhuys@gmail.com>
2025-11-24 21:55:33 -0500

    chore: Remove web1 package
    The React Router v7 web UI was causing significant development friction.
    This package will be rebuilt from scratch in the future.
    
    Recovery: git checkout 6118813 -- packages/web1

--- packages/web1/app/hooks/use-auth.tsx
+++
@@ -1,8 +0,0 @@
-import { useAuth as useClerkAuth } from "@clerk/clerk-react";
-
-/**
- * Auth hook that uses Clerk authentication
- */
-export function useAuth() {
-	return useClerkAuth();
-}
--- packages/web1/test-web1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /Users/hgeldenhuys/WebstormProjects/mw-core/packages/web1
-
-echo "Installing dependencies..."
-bun install
-
-echo -e "\nStarting web1..."
-bun dev
